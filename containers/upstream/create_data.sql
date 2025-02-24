78f5d8562636eed2e44ca0b028ce22e652c7a996
Shani Henriques <shanihenriques@gmail.com>
2025-02-24 21:42:38 +0000

    added docker compose and finish db initialisation script

--- containers/upstream/create_data.sql
+++ containers/upstream/create_data.sql
@@ -28,6 +28,34 @@ CREATE TABLE films.films_raw (
     vote_count BIGINT -- incase
 );
 
+COPY films.films_raw(
+    adult,
+    belongs_to_collection,
+    budget,
+    genres,
+    homepage,
+    id,
+    imdb_id,
+    original_language,
+    original_title,
+    overview,
+    popularity,
+    poster_path,
+    production_companies,
+    production_countries,
+    release_date,
+    revenue,
+    runtime,
+    spoken_languages,
+    status,
+    tagline,
+    title,
+    video,
+    vote_average,
+    vote_count,
+)
+FROM '/input_data/movies_metadata.csv' DELIMITER ',' CSV HEADER;
+
 -- CREDITS
 CREATE TABLE films.credits_raw (
     id INT PRIMARY KEY,
@@ -35,11 +63,17 @@ CREATE TABLE films.credits_raw (
     crew JSONB
 );
 
+COPY films.credits_raw(id, cast, crew)
+FROM '/input_data/credits_raw.csv' DELIMITER ',' CSV HEADER;
+
 -- KEYWORDS
 CREATE TABLE films.keywords_raw (
     id INT PRIMARY KEY,
     keywords JSONB
 );
+
+COPY films.keywords_raw(id, keywords)
+FROM '/input_data/keywords_raw.csv' DELIMITER ',' CSV HEADER;
 
 -- RATINGS
 CREATE TABLE films.ratings_raw (
@@ -51,9 +85,15 @@ CREATE TABLE films.ratings_raw (
     PRIMARY KEY (user_id, movie_id)  -- composite key to avoid duplicate ratings
 );
 
+COPY films.ratings_raw(user_id, movie_id, rating, timestamp)
+FROM '/input_data/ratings_raw.csv' DELIMITER ',' CSV HEADER;
+
 -- LINKS
 CREATE TABLE films.links_raw (
     movie_id INT PRIMARY KEY,  
     imdb_id VARCHAR(10) NOT NULL,  -- account for occasional leading zeros
     tmdb_id INT NOT NULL  
 );
+
+COPY films.links_raw(movie_id, imdb_id, tmdb_id)
+FROM '/input_data/links_raw.csv' DELIMITER ',' CSV HEADER;
